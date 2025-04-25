11238b8834debdd3243bc3a85a858f3331e4ba7e
jesocasi <jesocasi@espe.edu.ec>
2025-04-25 08:19:47 -0500

    se sube actualizacion

--- main.cpp
+++ main.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
+double sumar(double a, double b) {
+    return a + b;
+}
+
+
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    double num1;
+    double num2;
+    int opcion;
+
+    cout << "Porfavor ingresa el primer numero" << endl;
+    cin >> num1;
+    cout << "Porfavor ingresa el primer numero" << endl;
+    cin >> num2;
+    cout << "Que quieres hacer" << endl;
+    cin >>opcion;
+
+    switch(opcion){
+
+case 1:
+
+    cout << "la respuesta es: " <<sumar(num1,num2) <<endl;
+
+
+default:
+    cout << "Opción no válida." << endl;
+            break;
+    }
     return 0;
 }
